e120d8eb68117c50cd415c3cc349ccca1c967a63
刘月松 <liuyuesongde@163.com>
2018-08-30 16:47:03 +0800

    init with electron-vue, upgrade all deps and config

---
+++ scripts/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ports: {
+    renderer: 5890, // 开发阶段前端开发端口号
+    main: 5891, // 主进程端口
+    mainDebugger: 5892 // 主进程调试端口
+  }
+}
---
+++ scripts/webpack.main.config.js
@@ -0,0 +1,77 @@
+'use strict'
+
+process.env.BABEL_ENV = 'main'
+
+const path = require('path')
+const { dependencies } = require('../package.json')
+
+const BabiliWebpackPlugin = require('babili-webpack-plugin')
+
+const env = process.env.NODE_ENV || 'development'
+
+let mainConfig = {
+  entry: {
+    main: path.join(__dirname, '../src/main/index.js')
+  },
+  mode: env,
+  externals: [
+    ...Object.keys(dependencies || {})
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            formatter: require('eslint-friendly-formatter')
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.node$/,
+        use: 'node-loader'
+      }
+    ]
+  },
+  node: {
+    __dirname: process.env.NODE_ENV !== 'production',
+    __filename: process.env.NODE_ENV !== 'production'
+  },
+  output: {
+    filename: '[name].js',
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '../dist/electron')
+  },
+  plugins: [
+  ],
+  resolve: {
+    extensions: ['.js', '.json', '.node']
+  },
+  target: 'electron-main'
+}
+
+/**
+ * Adjust mainConfig for development settings
+ */
+if (process.env.NODE_ENV !== 'production') {
+  // mainConfig.plugins.push()
+}
+
+/**
+ * Adjust mainConfig for production settings
+ */
+if (process.env.NODE_ENV === 'production') {
+  mainConfig.plugins.push(
+    new BabiliWebpackPlugin()
+  )
+}
+
+module.exports = mainConfig
